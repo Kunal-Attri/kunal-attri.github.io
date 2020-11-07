26a96eca7b0950ecad48c2db04e6c8a973dda0db
Kunal Attri <35298757+Kunal-Attri@users.noreply.github.com>
2020-11-08 00:36:22 +0530

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // typing animation
+    var typed = new Typed(".unselectable-1", {
+        strings: [" CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer "],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+        showCursor: false,
+    });
+})
