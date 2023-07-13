f5a5cac99f9f406bffa165f29083191a566f22d9
Kunal Attri <attrikunal16@gmail.com>
2023-07-13 22:24:59 +0530

    added fruit quality detection app

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // typing animation
     var typed = new Typed(".unselectable-1", {
-        strings: [" CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer ", " CSE Undergrad ", " Coder ", " Content Tinkerer "],
+        strings: [" CSE Undergrad ", " Coder ", " Content Tinkerer ", " Cat Lover "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
